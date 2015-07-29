48e58a49c14b4db574c3dc05669a525c69314d01
Greg Fiorentino <gtfiorentino@gmail.com>
2015-07-29 02:37:52 -0400

    content-type on client

--- client/_config/superagent.js
+++ client/_config/superagent.js
@@ -2,4 +2,7 @@ import superagentDefaults from 'superagent-defaults';
 
 const superagent = superagentDefaults();
 
+superagent
+    .set('Content-Type', 'application/json');
+
 export default superagent;
