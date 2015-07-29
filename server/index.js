0f55c1da310dae089a1ae0fd14beca017e490050
Isaac K T Luxford <ikt.luxford@gmail.com>
2015-07-29 00:41:08 -0400

    Scaffolding the API with a POST route to /session, doing a very minimal auth check with credentials test/test

---
+++ server/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import logger from 'morgan';
+import errorhandler from 'errorhandler';
+import cors from 'cors';
+import jwt from 'express-jwt';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+import config from './config';
+import session from './resources/session';
+
+const app = express();
+const router = express.Router();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(logger('dev'));
+app.use(errorhandler());
+
+const protect = () => {
+  return jwt({secret: config.secret}).unless({path: ['/api/session']});
+};
+
+// set up API routing
+app.use('/api', protect(), router);
+router.post('/session', session.create);
+
+// Handle unauthorized errors gracefully.
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Invalid Token');
+  }
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Example app listening at http://${host}:${port}`);
+});
+
+export default app;
---
+++ server/resources/session/index.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import config from '../../config';
+
+export default {
+  create(req, res) {
+    const { username, password } = req.body;
+
+    /* obviously you would normally do a real look-up here */
+    if (username === 'test' && password === 'test') {
+      let token = jwt.sign({username: username}, config.secret, {
+        issuer: 'redux-demo',
+        expiresInMinutes: 60
+      });
+
+      res.send(token);
+    } else {
+      res.status(401).send('Authentication failure.');
+    }
+  }
+}
