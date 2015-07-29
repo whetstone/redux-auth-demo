f1e18fc763da4c7d0a89f2904007f82d6537eea5
Isaac K T Luxford <ikt.luxford@gmail.com>
2015-07-29 11:19:11 -0400

    configuring top-level component to wrap authentication from user-reducer around child routes

--- client/containers/app-container.js
+++ client/containers/app-container.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import * as actionCreators from '../action-creators';
+import { connect } from 'react-redux';
+
+import LoginFormComponent from '../components/login-form-component';
+
+@connect(state => ({
+  user: state.user,
+  loginForm: state.loginForm,
+}))
 
 export default class AppContainer {
-    render() {
-        return (
-            <section>
-                <header></header>
+  render() {
+    const { dispatch, user, loginForm, children } = this.props;
 
-                <section>
-                    {this.props.children}
-                </section>
-            </section>
-        );
-    }
+    const headerBlock = user.authenticated ?
+      <h1>Logged in as: {user.username}</h1> :
+      <h1>Not logged in.</h1>
+
+    const contentBlock = user.authenticated ?
+      children :
+      <LoginFormComponent loginForm={loginForm} {...bindActionCreators(actionCreators, dispatch)} />
+
+    return (
+      <section>
+        <header>
+          {headerBlock}
+        </header>
+
+        <section>
+          {contentBlock}
+        </section>
+      </section>
+    );
+  }
 }
