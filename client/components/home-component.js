ec6b6c9e278710a28d7854ac3717f954040d91db
Greg Fiorentino <gtfiorentino@gmail.com>
2015-08-02 16:27:08 -0400

    show username on home page

--- client/components/home-component.js
+++ client/components/home-component.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default class HomeComponent {
 
   render() {
-    const { tokenDeleted } = this.props;
+    const { tokenDeleted, user: { username } } = this.props;
 
     return (
       <div>
-        Welcome, authenticated user!
+        Welcome, authenticated user: {username}!
 
         <button onClick={tokenDeleted}>Log Out</button>
       </div>
